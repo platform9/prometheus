7d508b340974f8cbdd880af95122d187fcae6b4d
Rohitrajak1807 <rrajak@platform9.com>
2023-04-18 10:56:53 +0000

    use curl

--- get_go.sh
+++ get_go.sh
@@ -10,7 +10,7 @@ ARCH=amd64
 
 # Download the Go binary
 pushd $HOME || exit
-wget https://golang.org/dl/go$GO_VERSION.$OS-$ARCH.tar.gz
+curl -LO "https://golang.org/dl/go$GO_VERSION.$OS-$ARCH.tar.gz"
 # Extract the Go binary to $HOME/go
 tar -C $HOME -xzf go$GO_VERSION.$OS-$ARCH.tar.gz
 popd || exit

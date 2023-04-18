fc014b3c150deca24c96a7f47b4a118825e31e5a
Rohitrajak1807 <rrajak@platform9.com>
2023-04-18 10:53:02 +0000

    get go

---
+++ get_go.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#!/bin/bash
+
+# Set the Go version you want to install
+GO_VERSION=1.17.16
+
+# Set the OS and architecture
+OS=linux
+ARCH=amd64
+
+# Download the Go binary
+pushd $HOME || exit
+wget https://golang.org/dl/go$GO_VERSION.$OS-$ARCH.tar.gz
+# Extract the Go binary to $HOME/go
+tar -C $HOME -xzf go$GO_VERSION.$OS-$ARCH.tar.gz
+popd || exit
+# Add Go to the PATH environment variable
+echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc
+
+# Refresh the PATH environment variable
+source ~/.bashrc
+
+# Verify that Go has been installed
+go version

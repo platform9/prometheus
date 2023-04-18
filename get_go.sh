c12fce4efddf70204519412787b1f034bbb36367
Rohitrajak1807 <rrajak@platform9.com>
2023-04-18 14:52:38 +0000

    ok??

--- get_go.sh
+++ get_go.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 set -x
-
-# Set the Go version you want to install
-GO_VERSION=1.17.13
-
-# Set the OS and architecture
-OS=linux
-ARCH=amd64
-
 # Download the Go binary
 pushd $HOME || exit
-curl -LO "https://golang.org/dl/go$GO_VERSION.$OS-$ARCH.tar.gz"
-# Extract the Go binary to $HOME/go
-rm $GO_VERSION.$OS-$ARCH.tar.gz
-tar -C ./.local -xzf go$GO_VERSION.$OS-$ARCH.tar.gz
+curl -sL -o ~/.local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ~/.local/bin/gimme
 popd || exit
+eval "$(gimme 1.17)"
 
